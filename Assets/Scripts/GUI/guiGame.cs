e26200394c009d645192082a948e3722c81823db
Alex_A <ozzmotic@gmail.com>
2013-07-21 11:53:39 -0700

    added prefab of the logo and removed some extraneous code

--- Assets/Scripts/GUI/guiGame.cs
+++ Assets/Scripts/GUI/guiGame.cs
@@ -18,10 +18,6 @@ public class guiGame : MonoBehaviour {
 		float xMin = Screen.width/2 - ( crosshairImage.width/2 );
 		float yMin = Screen.height/2 - ( crosshairImage.height/2 );
 		GUI.DrawTexture(new Rect(xMin,yMin,crosshairImage.width,crosshairImage.height),crosshairImage);
-		
-		//PlayerBoost boost = gameObject.GetComponent<PlayerBoost>();
-		//ARV80_Rifle ARVammo = gameObject.GetComponent<ARV80_Rifle>();		
-		
 		GUI.Label(new Rect(Screen.width - 200,Screen.height-100,200,50),"Boosts: " + boost.currBoosts,HUDStyle_small);
 		GUI.Label(new Rect(Screen.width - 200,Screen.height-50,200,50),ARV.currentAmmo + " / " + ARV.currentSpareAmmo,HUDStyle_large);
 	}
