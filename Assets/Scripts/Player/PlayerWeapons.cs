578bc52a2643589ea15a79531f6170c360b5c380
Alex_A <ozzmotic@gmail.com>
2013-08-10 15:39:14 -0700

    Added weapon switching

--- Assets/Scripts/Player/PlayerWeapons.cs
+++ Assets/Scripts/Player/PlayerWeapons.cs
@@ -2,35 +2,76 @@
 using System.Collections;
 
 public class PlayerWeapons : MonoBehaviour {
+	
 	public GameObject AssaultRifle;
 	public GameObject SniperRifle;
-
+	public int currentWeapon = 1;
+	public int numWeapons = 2;
+	private int? nextWeapon = null;
 	private ARV80_Rifle assaultRifleScript;
 	private S107_SniperRifle sniperRifleScript;
-
-	private Weapon_Stats currentWeapon;
+	private Weapon_Stats currentWeaponScript;
 
 	protected void Start() {
 		assaultRifleScript = AssaultRifle.GetComponent<ARV80_Rifle>();
 		sniperRifleScript = SniperRifle.GetComponent<S107_SniperRifle>();
-		currentWeapon = assaultRifleScript;
-		currentWeapon.enabled = true;
+		
+		if( currentWeapon == 1 ) {
+			currentWeaponScript = assaultRifleScript;
+		} else if ( currentWeapon == 2 ) {
+			currentWeaponScript = sniperRifleScript;
+		}
+		currentWeaponScript.enabled = true;
+	}
+	
+	void Update() {
+		
+		if( Input.GetAxis("SwitchWeapon") > 0 ) {	
+			nextWeapon = currentWeapon + 1;
+			
+			if( nextWeapon <= numWeapons ) {
+				currentWeapon = nextWeapon.Value;
+			} else {
+				currentWeapon = 1;
+			}
+		} else if( Input.GetAxis("SwitchWeapon") < 0 ) {
+			nextWeapon = currentWeapon - 1;
+			
+			if( nextWeapon.Value > 0 ) {
+				currentWeapon = nextWeapon.Value;
+			} else {
+				currentWeapon = numWeapons;
+			}				
+		} else {
+			nextWeapon = null;	
+		}
+		
+		if ( nextWeapon.HasValue ) {
+			if( currentWeapon == 1 ) {
+				currentWeaponScript.enabled = false;
+				currentWeaponScript = assaultRifleScript;
+			} else if ( currentWeapon == 2 ) {
+				currentWeaponScript.enabled = false;
+				currentWeaponScript = sniperRifleScript;
+			}
+			currentWeaponScript.enabled = true;
+		}
 	}
 
 	public float GetWeaponCurrentAmmo() {
-		return currentWeapon.GetCurrentAmmo();
+		return currentWeaponScript.GetCurrentAmmo();
 	}
 
 	public float GetWeaponSpareAmmo() {
-		return currentWeapon.GetSpareAmmo();
+		return currentWeaponScript.GetSpareAmmo();
 	}
 	
 	public float GetWeaponMaxAmmo() {
-		return currentWeapon.GetMaxAmmo();
+		return currentWeaponScript.GetMaxAmmo();
 	}
 	
 	public float GetWeaponMaxSpare() {
-		return currentWeapon.GetMaxSpare();
+		return currentWeaponScript.GetMaxSpare();
 	}
 	
 	public bool IsFullCheck() {
@@ -40,6 +81,6 @@ public class PlayerWeapons : MonoBehaviour {
 	}
 	
 	public void SetWeaponCurrentAmmo( float ammo ) {
-		currentWeapon.SetCurrentAmmo( ammo );
+		currentWeaponScript.SetCurrentAmmo( ammo );
 	}
 }
