e8b177b4b2e634e27fd561938001b12fb876bd5f
Alex_A <ozzmotic@gmail.com>
2013-08-10 20:25:59 -0700

    Added working weapon pickups

---
+++ Assets/Scripts/Items/SniperRiflePickup.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SniperRiflePickup : MonoBehaviour {
+	
+	private PlayerWeapons weaponController;
+	private float respawnTime = 2.0f;
+	private float plusAmmo = 4.0f;
+	private float curAmmo;
+	
+	void OnTriggerEnter(Collider other) {
+		GameObject hitObject = other.gameObject;
+		if( hitObject.tag == "Player" && renderer.enabled ) {
+			weaponController = hitObject.GetComponent<PlayerWeapons>();
+
+			if( ! weaponController.HasWeaponCheck("SniperRifle") ) {
+				weaponController.AddWeapon("SniperRifle");
+				weaponController.SetCurrentWeapon("SniperRifle");
+				renderer.enabled = false;
+				StartCoroutine( PickupReturn() );
+			} else if ( ! weaponController.IsFullCheck("SniperRifle") ) {
+				curAmmo = weaponController.GetSniperCurrentAmmo();
+				weaponController.SetCurrentWeapon("SniperRifle");
+				weaponController.SetWeaponCurrentAmmo( curAmmo + plusAmmo );
+				renderer.enabled = false;
+				StartCoroutine( PickupReturn() );	
+			}
+		}
+	}
+	
+	IEnumerator PickupReturn() {
+		yield return new WaitForSeconds( respawnTime );
+		renderer.enabled = true;
+	}
+}
--- Assets/Scripts/Player/PlayerWeapons.cs
+++ Assets/Scripts/Player/PlayerWeapons.cs
@@ -5,59 +5,69 @@ public class PlayerWeapons : MonoBehaviour {
 	
 	public GameObject AssaultRifle;
 	public GameObject SniperRifle;
-	public int currentWeapon = 1;
-	public int numWeapons = 2;
-	private int? nextWeapon = null;
+	
+	private Weapon_Stats[] weaponSlots;  // player's weapon slots
+	private int totalWeapons = 3; // number of weapons in the game
+	private int availWeapons = 0; // number of weapons player owns
+	
+	//Start with weapon 0
+	private int currentWeapon = 0;
+	private int nextWeapon = 0;
+	
 	private ARV80_Rifle assaultRifleScript;
 	private S107_SniperRifle sniperRifleScript;
 	private Weapon_Stats currentWeaponScript;
-
-	protected void Start() {
+	
+	protected void Awake() {
+		weaponSlots = new Weapon_Stats[totalWeapons];
 		assaultRifleScript = AssaultRifle.GetComponent<ARV80_Rifle>();
 		sniperRifleScript = SniperRifle.GetComponent<S107_SniperRifle>();
 		
-		if( currentWeapon == 1 ) {
-			currentWeaponScript = assaultRifleScript;
-		} else if ( currentWeapon == 2 ) {
-			currentWeaponScript = sniperRifleScript;
+		// Populate list of weapons
+		for (int i=0; i<totalWeapons; ++i) {
+			weaponSlots[i] = null;
 		}
+		weaponSlots[0] = assaultRifleScript;
+		availWeapons = 1;
+		
+		// Set initial weapon
+		currentWeaponScript = weaponSlots[0];
 		currentWeaponScript.enabled = true;
 	}
 	
 	void Update() {
-		
-		if( Input.GetAxis("SwitchWeapon") > 0 ) {	
+		if( Input.GetAxis("SwitchWeapon") > 0 && availWeapons > 1 ) {
 			nextWeapon = currentWeapon + 1;
 			
-			if( nextWeapon <= numWeapons ) {
-				currentWeapon = nextWeapon.Value;
-			} else {
-				currentWeapon = 1;
+			if ( nextWeapon >= totalWeapons) {
+				nextWeapon = 0;
 			}
-		} else if( Input.GetAxis("SwitchWeapon") < 0 ) {
+			while ( weaponSlots[nextWeapon] == null ) {
+				++nextWeapon;
+			}
+		} else if( Input.GetAxis("SwitchWeapon") < 0 && availWeapons > 1 ) {
 			nextWeapon = currentWeapon - 1;
 			
-			if( nextWeapon.Value > 0 ) {
-				currentWeapon = nextWeapon.Value;
-			} else {
-				currentWeapon = numWeapons;
-			}				
+			if ( nextWeapon < 0 ) { 
+				nextWeapon = totalWeapons - 1;
+			}
+			
+			while ( weaponSlots[nextWeapon] == null) {
+				--nextWeapon;
+			}
 		} else {
-			nextWeapon = null;	
+			nextWeapon = currentWeapon;	
 		}
 		
-		if ( nextWeapon.HasValue ) {
-			if( currentWeapon == 1 ) {
-				currentWeaponScript.enabled = false;
-				currentWeaponScript = assaultRifleScript;
-			} else if ( currentWeapon == 2 ) {
-				currentWeaponScript.enabled = false;
-				currentWeaponScript = sniperRifleScript;
-			}
+		if ( nextWeapon != currentWeapon ) {
+			currentWeapon = nextWeapon;
+			currentWeaponScript.enabled = false;
+			currentWeaponScript = weaponSlots[currentWeapon];
 			currentWeaponScript.enabled = true;
 		}
 	}
-
+	
+	//These getters correspond to the currently equipped weapon
 	public float GetWeaponCurrentAmmo() {
 		return currentWeaponScript.GetCurrentAmmo();
 	}
@@ -73,14 +83,84 @@ public class PlayerWeapons : MonoBehaviour {
 	public float GetWeaponMaxSpare() {
 		return currentWeaponScript.GetMaxSpare();
 	}
-	
+		
 	public bool IsFullCheck() {
+		
 		if( GetWeaponCurrentAmmo() == GetWeaponMaxAmmo() && GetWeaponSpareAmmo() == GetWeaponMaxSpare() ) {
-			return true;
+				return true;
+			} else { return false; }
+	}
+	
+	//These getters are for specific weapons (to query ammo counts of weapons not currently equipped)
+	//New ones will need to be written as weapons are implemented
+	public float GetSniperCurrentAmmo() {
+		return sniperRifleScript.GetCurrentAmmo();
+	}
+	
+	public float GetSniperSpareAmmo() {
+		return sniperRifleScript.GetSpareAmmo();
+	}	
+	
+	public float GetSniperMaxAmmo() {
+		return sniperRifleScript.GetMaxAmmo();
+	}
+	
+	public float GetSniperMaxSpare() {
+		return sniperRifleScript.GetMaxSpare();	
+	}
+	
+	//Overloaded method for when a specific weapon's stats are requested
+	public bool IsFullCheck( string weapon ) {
+		
+		if( weapon == "SniperRifle" ) {
+			
+			if( GetSniperCurrentAmmo() == GetSniperMaxAmmo() && GetSniperSpareAmmo() == GetSniperMaxSpare() ) {
+				return true;
+			} else { return false; }				
+		} else { return false; }
+	}
+	
+	public bool HasWeaponCheck( string weapon ) {
+		
+		if( weapon == "AssaultRifle" ) {
+			
+			if( weaponSlots[0] == null ) {
+				return false;
+			} else { return true; }
+			
+		} else if( weapon == "SniperRifle" ) {
+			
+			if( weaponSlots[2] == null ) {
+				return false;	
+			} else { return true; }
+			
 		} else { return false; }
 	}
 	
 	public void SetWeaponCurrentAmmo( float ammo ) {
 		currentWeaponScript.SetCurrentAmmo( ammo );
 	}
+		
+	public void AddWeapon( string weapon ) {
+	
+		if( weapon == "SniperRifle" ) {
+			weaponSlots[2] = sniperRifleScript;
+			availWeapons++;	
+		}	
+	}	
+	
+	public void SetCurrentWeapon( string weapon ) {
+		
+		if( weapon == "AssaultRifle" ) {
+			currentWeapon = 0;
+			currentWeaponScript.enabled = false;
+			currentWeaponScript = weaponSlots[currentWeapon];
+			currentWeaponScript.enabled = true;
+		} else if( weapon == "SniperRifle" ) {
+			currentWeapon = 2;
+			currentWeaponScript.enabled = false;
+			currentWeaponScript = weaponSlots[currentWeapon];
+			currentWeaponScript.enabled = true;
+		}
+	}		
 }
