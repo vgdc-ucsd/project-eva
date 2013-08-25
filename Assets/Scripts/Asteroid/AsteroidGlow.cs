2b3d1d96831cf0279954c5db4620e934beb0e0a0
Alex_A <ozzmotic@gmail.com>
2013-08-25 13:11:48 -0700

    Asteroids are outlined if you are within cover range -- also, added a folder for shaders

---
+++ Assets/Scripts/Asteroid/AsteroidGlow.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidGlow : MonoBehaviour {
+	
+	private GameObject ParentAsteroid;
+	private Shader outlineShader;
+	private Shader defaultShader;
+	
+	// Use this for initialization
+	void Start () {
+		ParentAsteroid = transform.parent.gameObject;
+		outlineShader = Shader.Find( "Outlined/Silhouetted Diffuse" );
+		defaultShader = Shader.Find( "Diffuse" );
+	}
+	
+	void OnTriggerEnter(Collider other) {
+		if( other.tag == "Player" ) {
+			ParentAsteroid.renderer.material.shader = outlineShader;
+		}
+	}
+	
+	void OnTriggerExit(Collider other) {
+		if( other.tag == "Player" ) {
+			ParentAsteroid.renderer.material.shader = defaultShader;
+		}
+	}
+}
